e252b1244655a00b7321fa9c9d9f871ca0d7fc3e
MurrinB <britneemurrin@yahoo.com>
2021-05-02 16:56:10 -0600

    Used list of integars
    Used foreach to iterate through and divide a number by each number, used try/catch to signal error issues

---
+++ Assignments/IntegarLoop/IntegarLoop/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegarLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 5, 10, 15, 20, 25 }; // CREATE LIST OF INTEGARS
+            try // HELP CATCH USER ERRORS
+            {
+
+            foreach (int number in numbers) // ITERATE THROUGH NUMBER LIST
+            {
+                Console.WriteLine(number); // PRINT EACH NUMBER IN CONSOLE
+            }
+
+            Console.WriteLine("Pick a number to divide each number in this list by."); // REQUEST USER INPUT
+            int numberDivideBy = Convert.ToInt32(Console.ReadLine()); // SAVE USER INPUT NUMBER
+
+            foreach (int number in numbers) // ITERATE THROUGH NUMBER LIST 
+            {
+                int answer = number / numberDivideBy; // DIVID EACH NUMBER BY USER INPUT NUMBER
+                Console.WriteLine(number + " divided by " + numberDivideBy + " equals " + answer); // PRINT EQUATION OF EACH NUMBER IN CONSOLE
+            }
+
+            }
+            catch (FormatException ex) // CODE BLOCK IS FOR STRING INSTEAD OF INT ERROR
+            {
+                Console.WriteLine("Please write a whole number.");
+                
+            }
+            catch (DivideByZeroException ex) // CODE BLOCK IS FOR DIVIDING BY 0 ERROR
+            {
+                Console.WriteLine("Please don't divide by zero.");
+                
+            }
+            catch (Exception ex) // CODE BLOCK FOR ANY OTHER ERRORS
+            {
+                Console.WriteLine(ex);
+               
+            }
+            finally // CODE BLOCK SIGNALS THE END OF THE PROGRAM
+            {
+                Console.WriteLine("Program End!");
+            }
+
+            Console.WriteLine("The program has emerged from the try/catch block!"); // ALERTING THE USER THAT THE PROGRAM MADE IT THROUGH THE TRY/CATCH BLOCK
+
+            Console.ReadLine(); // KEEPS CONSOLE OPEN
+        }
+    }
+}
