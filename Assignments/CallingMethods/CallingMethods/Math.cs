740385ff7d23ed3f7802970d6b329bb03e57d989
MurrinB <britneemurrin@yahoo.com>
2021-05-02 23:37:11 -0600

    Created class with three methods
    Created three methods with one integer parameter. Called each method in main program

---
+++ Assignments/CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Math // CREATED CLASS CALLED MATH AND MADE IT PUBLIC TO USE
+    {
+
+        public static int AddFive(int x) // CREATED ADDFIVE METHOD WITH ONE INT PARAMETER AND MADE IT PUBLIC
+        {
+            return x + 5; // ADD FIVE TO INT GIVEN ARGUMENT 
+        }
+
+        public static int SubtractFive(int x) // CREATED SUBTRACTFIVE METHOD WITH ONE INT PARAMETER AND MADE IT PUBLIC
+        {
+            int answer = x - 5; // SUBTRACT FIVE FROM GIVEN ARGUMENT, MADE THE ANSWER A VARIABLE TO RETURN TO SEE IT DONE BOTH WAYS
+            return answer;
+        }
+
+        public static int MultiplyFive(int x) // CREATED MULTIPLYFIVE METHOD WITH ONE INT PARAMETER AND MADE IT PUBLIC
+        {
+            int answer = x * 5; // MULTIPLY BY FIVE TO GIVEN ARGUMENT
+            return answer;
+        }
+        }
+
+    }
+  
+
---
+++ Assignments/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter in a random number."); // REQUEST USER INPUT
+            int number = Convert.ToInt32(Console.ReadLine()); // SAVE INTEGER INPUT
+
+            int math = Math.AddFive(number); // CALL ADDFIVE FUNCTION WITH USER INPUT VARIABLE
+
+            int mathTwo = Math.SubtractFive(number); // CALL SUBTRACTFIVE FUNCTION WITH USER INPUT VARIABLE
+
+            int mathThree = Math.MultiplyFive(number); // CALL MULTIPLYFIVE FUNCTION WITH USER INPUT VARIABLE
+
+            Console.WriteLine("Your number plus 5 equals " + math +
+                "\nYour number subtracted by 5 equals " + mathTwo +
+                "\nYour number multiplied by 5 equals " + mathThree); // DISPLAY ALL ANSWERS FROM METHOD CALLS
+
+            Console.ReadLine(); // LEAVES CONSOLE OPEN
+        }
+    }
+}
