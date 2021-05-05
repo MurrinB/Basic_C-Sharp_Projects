4409f87f41e168cd562ff8374dcd8d12e2ca95ea
MurrinB <britneemurrin@yahoo.com>
2021-05-04 22:15:52 -0600

    Create multiple methods with same name
    Each method requires a different kind of variable, calls each method in main()

---
+++ Assignments/IntegerMethod/IntegerMethod/MathMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerMethod
+{
+    public class MathMethods // CREATE A CLASS
+    {
+        public static int MathOperation(int x) // CREATE METHOD WITHIN CLASS THAT PASSES AN INTEGER THROUGH
+        {
+            return x / 2; // RETURNS AN INTEGER WHEN CALLED IN PROGRAM
+        }
+
+        public static int MathOperation(decimal x) // CREATE METHOD THAT IS THE SAME NAME BUT HAS A DIFFERENT REQUIREMENT
+        {
+            int answer = Convert.ToInt32(x * 2); // ALSO HAS A DIFFERENT MATH FUNCTION AND RETURNS AN INTEGER, NOT A DECIMAL
+            return answer;
+        }
+
+        public static int MathOperation(string x) // CREATE METHOD THAT IS THE SAME NAME BUT DIFFERENT REQUIREMENT
+        {
+            int answer = Convert.ToInt32(x) + 10; // CONVERTS STRING VARIABLE TO INT AND MULTIPLIES BY TEN
+            return answer; // RETURN ANSWER AS AN INTEGER 
+        }
+    }
+}
---
+++ Assignments/IntegerMethod/IntegerMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Division = MathMethods.MathOperation(10); // CALLS DIVIDETWO METHOD AND PASSES IN 10
+            Console.WriteLine("The answer to 10 divided by 2 is " + Division); // DISPLAYS IN CONSOLE ANSWER FROM METHOD
+
+            decimal Multiplication = MathMethods.MathOperation(1.25m); // CALLS METHOD WITH SAME NAME BUT REQUIRES A DECIMAL 
+            Console.WriteLine("The answer to 1.25 times 2 as an integer is " + Multiplication); // DISPLAYS IN CONSOLE ANSWER FROM METHOD
+
+            int Addition = MathMethods.MathOperation("15"); // CALLS METHOD WITH SAME NAME BUT REQUIRES STRING VARIABLE
+            Console.WriteLine("The answer to the given string of 15 plus 10 is " + Addition); // DISPLAYS IN CONSOLE INTEGER ANSWER FROM MATH EQUATION 
+
+            Console.ReadLine(); // KEEPS CONSOLE OPEN
+        }
+    }
+}
