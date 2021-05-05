40d99340c82c7d8684f8252ea4ceb1c7367a3c1b
MurrinB <britneemurrin@yahoo.com>
2021-05-04 23:29:27 -0600

    optional parameter
    create two methods to let there be an optional parameter

---
+++ Assignments/TwoIntegerMethod/TwoIntegerMethod/MathMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntegerMethod
+{
+    public class MathMethod
+    {
+        public static int MathOperation(int a) // CREATE METHOD WITH ONE PARAMETER
+        {
+            return a + 10; // SEND BACK VARIABLE + 10
+        }   
+
+        public static int MathOperation(int a, int b) // CREATE SAME METHOD WITH TWO PARAMETERS
+        {
+            return a + b + 10; // SEND BACK SUM OF BOTH VARIABLES AND 10
+        }
+    }
+}
---
+++ Assignments/TwoIntegerMethod/TwoIntegerMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntegerMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please pick a number to add by ten.");
+            //int numOne = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please pick a second number to add to the equation. Feel free to leave blank.");
+            //int numTwo = Convert.ToInt32(Console.ReadLine());
+            //int answer = MathMethod.MathOperation(numOne, numTwo);
+            //Console.WriteLine("The answer to this equation is " + answer);
+
+            int answer = MathMethod.MathOperation(10); // CALL METHOD WITH ONE ARGUMENT
+            int answerTwo = MathMethod.MathOperation(10, 12); // CALL METHOD WITH TWO ARGUMENTS 
+            Console.WriteLine("Answer with one variable: " + answer + "\nAnswer with two variables: " + answerTwo); // DISPLAY BOTH ANSWERS FROM THE TWO CALLS
+            
+
+            Console.ReadLine(); // KEEP CONSOLE OPEN
+
+        }
+    }
+}
