32cf737acf9e0f8480a9392d7bb8730136bc9244
MurrinB <britneemurrin@yahoo.com>
2021-05-05 23:05:26 -0600

    Create void method
    Created void method and called it in Main()

---
+++ Assignments/ChallengeInteger/ChallengeInteger/IntegersClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeInteger
+{
+    public class IntegersClass // CREATE NEW CLASS
+    {
+        public void IntegersMethod(int x, int y) // CREATE METHOD REQUIRING TWO INTERGER PARAMETERS
+        {
+            int answer = x * 5; // MATH OPERATION ON PARAMETER ONE
+            Console.WriteLine(y); // DISPLAY PARAMETER TWO IN CONSOLE
+
+        }
+
+    }
+}
---
+++ Assignments/ChallengeInteger/ChallengeInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegersClass instantiateClass = new IntegersClass(); // INSTANTIATE CLASS
+            instantiateClass.IntegersMethod(4, 5); // CALL METHOD AND PASS IN TWO INTEGERS
+            instantiateClass.IntegersMethod(x: 4, y: 5); // CALL METHODS SPECIFYING PARAMETERS BY NAME
+            Console.ReadLine(); // KEEPS CONSOLE OPEN
+        }
+    }
+}
