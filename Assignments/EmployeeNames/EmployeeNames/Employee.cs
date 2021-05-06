100232d61a9d49423c8632f7cc3feb4101997cb0
MurrinB <britneemurrin@yahoo.com>
2021-05-06 13:57:55 -0600

    Added an abstract class and virtual method
    Made additions to Employee class

--- Assignments/EmployeeNames/EmployeeNames/Employee.cs
+++ Assignments/EmployeeNames/EmployeeNames/Employee.cs
@@ -9,5 +9,11 @@ namespace EmployeeNames
     public class Employee : Person // CREATE EMPLOYEE CLASS WITH INHERITANCE TO PERSON
     {
         public int id { get; set; } // CREATE INT PROPERTY
+
+        public override void SayName() // MADE ADJUSTMENTS TO INHERITED METHOD WITH OVERRIDE
+        {
+            Console.WriteLine("Here is the employee's name."); // ADDITION TO METHOD
+            base.SayName();
+        }
     }
 }
--- Assignments/EmployeeNames/EmployeeNames/Person.cs
+++ Assignments/EmployeeNames/EmployeeNames/Person.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace EmployeeNames
 {
-    public class Person // CREATE CLASS PERSON
+    public abstract class Person // CREATE ABSTRACT CLASS PERSON, CANNOT BE INSTANTIATED 
     {
         public string FirstName { get; set; } // CREATE STRING PROPERTIES
         public string LastName { get; set; }
 
 
-        public void SayName() { // CREATE METHOD TO WRITE OUT NAME IN CONSOLE
+        public virtual void SayName() { // CREATE METHOD TO WRITE OUT NAME IN CONSOLE,  USED VIRTUAL TO BE ABLE TO OVERRIDE IN INHERITED CLASSES
 
         Console.WriteLine("Name: " + FirstName + " " + LastName);
             
