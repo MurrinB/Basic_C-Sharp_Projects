a225d32a6e3f1d2a291874195ddc2ff17c4245ef
MurrinB <britneemurrin@yahoo.com>
2021-05-06 21:45:22 -0600

    Added an interface
    with an interface method

--- Assignments/BLackjackGame/BLackjackGame/TwentyOneGame.cs
+++ Assignments/BLackjackGame/BLackjackGame/TwentyOneGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BLackjackGame
 {
-    public class TwentyOneGame : Game
+    public class TwentyOneGame : Game, IWalkAway
     {
         public override void Play()
         {
@@ -17,6 +17,11 @@ namespace BLackjackGame
         {
             Console.WriteLine("21 Players:");
             base.ListPlayers();
+        }
+
+        public void WalkAway(Player player) // IMPLEMENTATION OF INTERFACE IWALKAWAY METHOD
+        {
+            throw new NotImplementedException();
         }
 
     }
