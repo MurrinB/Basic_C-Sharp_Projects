40a5c814ae5315bf23eec80f1c7e47c0f7e71062
MurrinB <britneemurrin@yahoo.com>
2021-05-08 10:57:37 -0600

    Tried to debug catch issue

--- Assignments/DaysOfTheWeek/DaysOfTheWeek/Program.cs
+++ Assignments/DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -22,10 +22,11 @@ namespace DaysOfTheWeek
         {
             List<string> Days = new List<string>(); // CREATE EMPTY STRING TO ASSIGN ENUM VALUE TO 
             Console.WriteLine("What day of the week is it?"); // PROMPT USER INPUT
-            string answer = Console.ReadLine(); // SAVE USER INPUT AS VARIABLE ANSWER
+           
 
             try // TRY TO CATCH ERRORS IF USER DOESN'T TYPE A PROPER DAY OF THE WEEK
             {
+                string answer = Console.ReadLine(); // SAVE USER INPUT AS VARIABLE ANSWER
                 foreach (string day in Enum.GetNames(typeof(Day))) // PARSE THROUGH ENUM DAY
                 {
                     if (day == answer) // COMPARE USER ANSWER TO ENUM
@@ -39,7 +40,7 @@ namespace DaysOfTheWeek
                 }
             
             }
-            catch (Exception) // GIVE ERROR MESSAGE IF USER TYPES ANYTHING ELSE
+            catch (System.Exception) // GIVE ERROR MESSAGE IF USER TYPES ANYTHING ELSE
             {
                 Console.WriteLine("Please enter a day of the week.");
             }
