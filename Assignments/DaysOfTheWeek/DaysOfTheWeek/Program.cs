960bc3ff7de201ad84721f9e5de8162c43a8a5f6
MurrinB <britneemurrin@yahoo.com>
2021-05-08 10:02:29 -0600

    Create Enum and parse through

---
+++ Assignments/DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        public enum Day // CREATE ENUM OF DAYS OF THE WEEK
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            List<string> Days = new List<string>(); // CREATE EMPTY STRING TO ASSIGN ENUM VALUE TO 
+            Console.WriteLine("What day of the week is it?"); // PROMPT USER INPUT
+            string answer = Console.ReadLine(); // SAVE USER INPUT AS VARIABLE ANSWER
+
+            try // TRY TO CATCH ERRORS IF USER DOESN'T TYPE A PROPER DAY OF THE WEEK
+            {
+                foreach (string day in Enum.GetNames(typeof(Day))) // PARSE THROUGH ENUM DAY
+                {
+                    if (day == answer) // COMPARE USER ANSWER TO ENUM
+                    {
+                        Days.Add(day); // ADD ENUM VALUE TO EMPTY LIST IF IT MATCHES
+                        for (int i = 0; i < Days.Count; i++) // PARSE THROUGH DAYS LIST
+                        {
+                            Console.WriteLine(Days[i]); // DISPLAY DAYS LIST IN CONSOLE 
+                        }
+                    }
+                }
+            
+            }
+            catch (Exception) // GIVE ERROR MESSAGE IF USER TYPES ANYTHING ELSE
+            {
+                Console.WriteLine("Please enter a day of the week.");
+            }
+
+
+
+            Console.ReadLine(); // KEEPS CONSOLE OPEN
+    
+                
+        }
+    }
+}
