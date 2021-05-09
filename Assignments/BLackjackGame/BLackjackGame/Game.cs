3ce7e8c2419402184632d5d97c7df03f324291c2
MurrinB <britneemurrin@yahoo.com>
2021-05-08 19:21:35 -0600

    Finished the base of the BlackJack game
    So much was added here, game successfully runs, bugs could be worked on

--- Assignments/BLackjackGame/BLackjackGame/Game.cs
+++ Assignments/BLackjackGame/BLackjackGame/Game.cs
@@ -8,10 +8,13 @@ namespace BLackjackGame
 {
     public abstract class Game // ABSTRACT LOCKS THE CLASS SO YOU CAN NEVER CREATE AN INSTANCE OF THE CLASS
     {
-        public List<Player> Players { get; set; }
-        public string Name { get; set; }
-        public string Dealer { get; set; }
+        private List<Player> _players = new List<Player>();
+        private Dictionary<Player, int> _bets = new Dictionary<Player, int>();
 
+        public List<Player> Players { get { return _players; } set { _players = value; } }
+        public string Name { get; set; }
+        public Dictionary<Player, int> Bets { get { return _bets; } set { _bets = value; } }
+       
         public abstract void Play(); // ANY CLASS INHERITING GAME CLASS WILL HAVE THIS METHOD
         
         public virtual void ListPlayers() // METHOD GETS INHERITED BY INHERITING CLASS BUT CAN BE OVERRIDEN
--- Assignments/BLackjackGame/BLackjackGame/Program.cs
+++ Assignments/BLackjackGame/BLackjackGame/Program.cs
@@ -9,18 +9,34 @@ namespace BLackjackGame
     class Program
     {
         static void Main(string[] args)
-        { 
-            Deck deck = new Deck();
-            //deck.Shuffle(3);
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
 
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
 
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank); // CONSTRUCTOR
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivleyPlaying = true;
+                while (player.isActivleyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
             Console.ReadLine();
+
+          
         }
 
     }
