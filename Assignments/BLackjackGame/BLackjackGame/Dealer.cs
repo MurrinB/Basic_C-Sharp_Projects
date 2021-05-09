4c5790123a8f4a120adeecc01ba115fde13793d0
MurrinB <britneemurrin@yahoo.com>
2021-05-08 21:50:02 -0600

    Added a txt log
    Added a log to get a txt file log of all cards dealt

--- Assignments/BLackjackGame/BLackjackGame/Dealer.cs
+++ Assignments/BLackjackGame/BLackjackGame/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BLackjackGame
 {
@@ -15,7 +16,12 @@ namespace BLackjackGame
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Britnee Murrin\OneDrive\Desktop\FolderA\log.txt", true))
+            {
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
