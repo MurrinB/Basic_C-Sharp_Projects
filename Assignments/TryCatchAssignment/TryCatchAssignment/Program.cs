1ca352020e8ba9f94b94f0a0983e2f6f7b373a9f
MurrinB <britneemurrin@yahoo.com>
2021-05-09 22:03:11 -0600

    Catch Exceptions
    Gives error messages when exceptions occur

---
+++ Assignments/TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your age?");
+            bool validAnswer = false;
+            while (!validAnswer)
+            {
+            try
+                {
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    DateTime birthyear = DateTime.Now.AddYears(-age);
+                    Console.WriteLine("The year you were born was " + birthyear.Year);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please write a number.");
+                    validAnswer = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An error occurred.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            
+            
+            
+            Console.ReadLine();
+
+        }
+    }
+}
