219e929287206b468107df0c667f3847f6fbd667
MurrinB <britneemurrin@yahoo.com>
2021-05-09 19:39:59 -0600

    Chain Constructors

---
+++ Assignments/ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Person 
+    {
+        public Person(string name) : this (name, "F") { } // CHAIN CONSTRUCT PERSON WITH ONE PARAMETER AND GIVE AUTO GEN VARIABLE FOR PARAMETER TWO
+
+        public Person(string name, string gender) // CREATE PERSON CONSTRUCT WITH TWO PARAMETERS
+        {
+            Name = name;
+            Gender = gender;
+        }
+        public string Name; // ASSIGN VARIABLES
+        public string Gender;
+    }
+}
---
+++ Assignments/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fName = "Bob"; // CREATE CONSTANT VARIABLE 
+            const string lName = "Smith"; // SECOND CONSTANT VARIABLE
+            var email = "bob.smith@msn.com"; // CREATE VARIABLE USING VAR 
+            Console.WriteLine(fName + " " + lName + "\nThis email was created using var: " + email); // DISPLAY CANSTANT VARIABLE AND VAR VARIABLE ALL STRING VARIABLES
+
+            Person person = new Person("Britnee"); // INSTANTIATE CLASS PERSON WITH ONE VARIABLE NAME
+            Console.WriteLine(person.Name + " " + person.Gender); // DISPLAY PERSON INSTANCE WITH GENDER VARIABLE ADDED BECAUSE OF CONSTRUCT CHAINING 
+
+            Console.ReadLine();
+
+        }
+    }
+}
