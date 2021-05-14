bd13278ee1efbf0f5bb37f8a49f28fb1a8710976
MurrinB <britneemurrin@yahoo.com>
2021-05-14 13:33:38 -0600

    Created .Net Core project for scores

---
+++ DotNetCore/Scores/Scores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back " + (uName) + " Today is " + (date);
+            Console.WriteLine(msg);
+
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
