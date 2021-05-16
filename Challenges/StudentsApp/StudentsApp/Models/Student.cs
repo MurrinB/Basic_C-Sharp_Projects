cc19413be2c66a433b76a4d7b474ea229b6d1d98
MurrinB <britneemurrin@yahoo.com>
2021-05-15 21:17:30 -0600

    Created Code First Program

---
+++ Challenges/StudentsApp/StudentsApp/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsApp.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public Grade Grade { get; set; }
+    }
+
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ Challenges/StudentsApp/StudentsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using StudentsApp.Models;
+using System;
+
+namespace StudentsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var studentdb = new SchoolContext())
+            {
+                var student = new Student() { StudentID = 32, StudentName = "Bill"  };
+                var grade = new Grade() { GradeId = 98, GradeName = "Test1", Section = "Math" };
+
+                studentdb.Students.Add(student);
+                studentdb.SaveChanges();
+            }
+        }
+    }
+}
